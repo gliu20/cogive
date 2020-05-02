5ee37d5eabef54bbe78b9631eced866a8f195a99
gliu20 <gliu10000@gmail.com>
2020-05-02 11:10:41 -0400

    add defered searching

--- src/HospitalLocator.svelte
+++ src/HospitalLocator.svelte
@@ -1,8 +1,31 @@
 <script>
     const database = firebase.database();
     const hospitalsRef = database.ref("hospitals");
+    /**/
+
+    let country;
+    let state;
+    let city;
+
+    let status;
 
 
-    
+    const deferrables = debounce(1000,function () {
+        status = "Found"
+    },function () {
+        status = `Searching for ${country} in ${state} in ${city}...`;
+    });
+
+    $: deferrables(country,state,city)
 
 </script>
+
+
+{status}
+
+<input bind:value={country}>
+<input bind:value={state}>
+<input bind:value={city}>
+
+
+
