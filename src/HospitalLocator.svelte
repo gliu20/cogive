bee49f7ab8cea212b61114e1b951e5f6d49d6fa7
Azal-Amer <58004047+Azal-Amer@users.noreply.github.com>
2020-05-03 17:26:11 -0500

    got lat and long working

--- src/HospitalLocator.svelte
+++ src/HospitalLocator.svelte
@@ -1,13 +1,39 @@
+
+ 
 <script>
-    const database = firebase.database();
-    const hospitalsRef = database.ref("hospitals");
-    var longyHospital= localStorage.getItem("longitudeLocalStorage");  
-    var latyHospital= localStorage.getItem("latitudeLocalStorage");
-    import latitude from './main.js';
-    import longitude from './main.js';
-    latHospital = latitude;
-    longHospital = longitude;
-    console.log(latHospital);
+function geoFindMe() {
+
+	const status = document.querySelector('#status');
+	const mapLink = document.querySelector('#map-link');
+  
+	mapLink.href = '';
+	mapLink.textContent = '';
+  
+	function success(position) {
+	  latHospital  = position.coords.latitude;
+	  longHospital = position.coords.longitude;
+  
+	  status.textContent = '';
+	  mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+	  mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+	}
+  
+	function error() {
+	  status.textContent = 'Unable to retrieve your location';
+	}
+  
+	if(!navigator.geolocation) {
+	  status.textContent = 'Geolocation is not supported by your browser';
+	} else {
+	  status.textContent = 'Locating…';
+	  navigator.geolocation.getCurrentPosition(success, error);
+	}
+  
+  }
+  
+    geoFindMe();
+    var latHospital = latitude;
+    var longHospital = longitude;
     /**/
 
     let country = "";
@@ -26,6 +52,7 @@
             .then(function (snapshot) {
             searchResult = JSON.stringify(snapshot.val());
         })
+    
 
         status = `Found ${searchResult}`;
     }, function () {
@@ -112,6 +139,7 @@
     }
 
     displayHospitals();
+    console.log(latHospital + " " + longHospital);
 
 </script>
 
