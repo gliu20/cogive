7bb791c61b9a752949b1f8d63a96908c7139be43
George <gliu10000@gmail.com>
2020-05-04 13:55:23 -0400

    remove map code

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 
-export default app;
 
-var geoRun = true;
-
-
-localStorage.setItem("latitudeLocalStorage", latitude);  
-localStorage.setItem("longitudeLocalStorage", longitude);  
-export var latitude;
-export var longitude;
 import App from './App.svelte';
 
 var app = new App({
 	target: document.body
 });
+
+export default app;
