81756912369056e6853a29a3728886849b4c2fa1
gliu20 <gliu10000@gmail.com>
2020-05-04 12:33:12 -0400

    fix cache bug

--- src/libs/cache.js
+++ src/libs/cache.js
@@ -1,5 +1,6 @@
 const cache = {};
 
+cache.storage = {};
 cache.maxAge = 24 * 60 * 60 * 1000; // 1 day
 
 cache.set = function (key,value) {
@@ -12,12 +13,12 @@ cache.set = function (key,value) {
 cache.get = function (key) {
     const value = cache.storage[key];
 
-    if (Date.now() - value.timestamp > cache.maxAge) {
-        // data is too old
+    if (!value) {
+        // cache doesn't have the data
         return { success:false };
     }
-    else if (!cache.storage[key]) {
-        // cache doesn't have the data
+    else if (Date.now() - value.timestamp > cache.maxAge) {
+        // data is too old
         return { success:false };
     }
     else {
