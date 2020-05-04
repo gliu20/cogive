21c1d777302697bc857f07fcc9c7d41e967ce142
gliu20 <gliu10000@gmail.com>
2020-05-04 13:43:36 -0400

    add error handle and formatting

--- src/libs/hospitalLocator.js
+++ src/libs/hospitalLocator.js
@@ -7,19 +7,19 @@ const hospitalLocator = {};
  */
 hospitalLocator._getLocationFromIp = async function (ipAddress) {
     const requestUrl = `https://ipapi.co/json/`;
-    const locationDetails = await fetch(requestUrl,{});
+    const locationDetails = await fetch(requestUrl, {});
     const locationDetailsJson = await locationDetails.json();
 
-    const {country,city,region,latitude,longitude} = locationDetailsJson;
+    const { country, city, region, latitude, longitude } = locationDetailsJson;
 
-    return {country,city,region,latitude,longitude};
+    return { country, city, region, latitude, longitude };
 }
 
 
 /**
  * Returns object with latitude and longitude properties
  */
-hospitalLocator.getLocationFromBrowser = function () {
+hospitalLocator._getLocationFromBrowser = function () {
     const cachedLocation = cache.get("location");
 
     const options = {
@@ -28,7 +28,7 @@ hospitalLocator.getLocationFromBrowser = function () {
         maximumAge: 60000 // 60 seconds
     };
 
-    return new Promise (function (resolve,reject) {
+    return new Promise(function (resolve, reject) {
         if (cachedLocation.success) {
             resolve(cachedLocation.data);
         }
@@ -38,27 +38,43 @@ hospitalLocator.getLocationFromBrowser = function () {
             reject();
         }
 
-        navigator.geolocation.getCurrentPosition(onSuccess,onError,options);
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 
 
-        function onSuccess (position) {
+        function onSuccess(position) {
             if (!position.coords) {
                 reject();
             }
 
             const latitude = position.coords.latitude;
             const longitude = position.coords.longitude;
-            
-            cache.set("location", {longitude,latitude})
-            resolve({longitude,latitude})
+
+            cache.set("location", { longitude, latitude })
+            resolve({ longitude, latitude })
         }
 
-        function onError (err) {
+        function onError(err) {
             reject()
         }
     });
 }
 
+
+hospitalLocator.getLocationFromBrowser = async function () {
+
+    let location;
+
+    try {
+        location = await hospitalLocator._getLocationFromBrowser();
+    }
+    catch (err) {
+        // oh know but we are not affected
+        // we just give default coordinate and let the system deal with it
+        location = {latitude:41,longitude:-74};
+    }
+
+    return location;
+}
 
 hospitalLocator.getHospitalsNearby = async function () {
 
@@ -74,10 +90,14 @@ hospitalLocator.getHospitalsNearby = async function () {
     const longitude = position.longitude || 0;
     const requestUrl = `https://www.overpass-api.de/api/interpreter?data=[out:json][timeout:25];nwr(around:10000,${latitude},${longitude})[%22amenity%22=%22hospital%22];out%20center;`;
 
-    const hospitalList = await fetch(requestUrl,{});
+    const hospitalList = await fetch(requestUrl, {});
     const hospitalListJson = await hospitalList.json();
 
-    cache.set("hospitalList",hospitalListJson);
+    cache.set("hospitalList", hospitalListJson);
 
     return hospitalListJson;
+}
+
+hospitalLocator.generateGeoJson (hospitalListJson) {
+    // TODO
 }
