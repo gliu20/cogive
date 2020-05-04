680e3c3ad6b2091e7cba6a81f1825e46c181d74a
gliu20 <gliu10000@gmail.com>
2020-05-04 11:26:54 -0400

    add hospital locator api

--- src/HospitalLocator.svelte
+++ src/HospitalLocator.svelte
@@ -1,38 +1,36 @@
-
- 
 <script>
 
-function geoFindMe() {
+    function geoFindMe() {
 
-	const status = document.querySelector('#status');
-	const mapLink = document.querySelector('#map-link');
-  
-	mapLink.href = '';
-	mapLink.textContent = '';
-  
-	function success(position) {
-	  latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
-      
-  
-	  status.textContent = '';
-	  mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-	  mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-	}
-  
-	function error() {
-	  status.textContent = 'Unable to retrieve your location';
-	}
-  
-	if(!navigator.geolocation) {
-	  status.textContent = 'Geolocation is not supported by your browser';
-	} else {
-	  status.textContent = 'Locating…';
-	  navigator.geolocation.getCurrentPosition(success, error);
-	}
-  
-  }
-  
+        const status = document.querySelector('#status');
+        const mapLink = document.querySelector('#map-link');
+
+        mapLink.href = '';
+        mapLink.textContent = '';
+
+        function success(position) {
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+
+
+            status.textContent = '';
+            mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+            mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+        }
+
+        function error() {
+            status.textContent = 'Unable to retrieve your location';
+        }
+
+        if (!navigator.geolocation) {
+            status.textContent = 'Geolocation is not supported by your browser';
+        } else {
+            status.textContent = 'Locating…';
+            navigator.geolocation.getCurrentPosition(success, error);
+        }
+
+    }
+
     geoFindMe();
     console.log(latitude + "" + longitude)
 
@@ -53,9 +51,9 @@ function geoFindMe() {
             .child(city)
             .once("value")
             .then(function (snapshot) {
-            searchResult = JSON.stringify(snapshot.val());
-        })
-    
+                searchResult = JSON.stringify(snapshot.val());
+            })
+
 
         status = `Found ${searchResult}`;
     }, function () {
@@ -153,6 +151,6 @@ function geoFindMe() {
 <input bind:value={state}>
 <input bind:value={city}>
 
-<br/>
+<br />
 
 <p id="hospitals"></p>
---
+++ src/libs/hospitalLocator.js
@@ -0,0 +1,58 @@
+const hospitalLocator = {};
+
+/**
+ * doesn't work b/c the api seems to be disabled
+ */
+hospitalLocator._getLocationFromIp = async function (ipAddress) {
+    const requestUrl = `https://ipapi.co/json/`;
+    const locationDetails = await fetch(requestUrl,{});
+    const locationDetailsJson = await locationDetails.json();
+
+    const {country,city,region,latitude,longitude} = locationDetailsJson;
+
+    return {country,city,region,latitude,longitude};
+}
+
+
+/**
+ * Returns object with latitude and longitude properties
+ */
+hospitalLocator._getLocationFromBrowser = function () {
+    return new Promise (function (resolve,reject) {
+        if (!navigator.geolocation) {
+            // unsupported
+            reject();
+        }
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+            if (!position.coords) {
+                reject();
+            }
+
+            const latitude = coords.latitude;
+            const longitude = coords.longitude;
+            
+            resolve({longitude,latitude})
+        },
+            reject(position)
+        ),{
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 60000 // 60 seconds
+        };
+    });
+}
+
+
+hospitalLocator.getHospitalsNearby = async function () {
+    const position = await hospitalLocator._getLocationFromBrowser();
+    
+    const latitude = position.latitude || 0;
+    const longitude = position.longitude || 0;
+    const requestUrl = `https://www.overpass-api.de/api/interpreter?data=[out:json][timeout:25];nwr(around:10000,${latitude},${longitude})[%22amenity%22=%22hospital%22];out%20center;`;
+
+    const hospitalList = await fetch(requestUrl,{});
+    const hospitalListJson = await hospitalList.json();
+
+    return hospitalListJson;
+}
