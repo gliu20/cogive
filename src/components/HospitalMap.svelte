97dc3a60b05cfcd904edd47a16c6d47af994a3c5
gliu20 <gliu10000@gmail.com>
2020-05-04 13:42:58 -0400

    add hospital marker code

--- src/components/HospitalMap.svelte
+++ src/components/HospitalMap.svelte
@@ -11,26 +11,26 @@
 
 <script>
     import { onMount } from 'svelte';
-    
+
     // keep track of what's loaded;
     // we need this to initialize map only when 
     // everything is ready
     let mounted = false;
     let mapboxLoaded = false;
     let location;
-    let map;
+    window.map="blah";
 
     onMount(() => {
         mounted = true;
         onReady();
     })
 
-    function onMapboxLoaded () {
+    function onMapboxLoaded() {
         mapboxLoaded = true;
         onReady();
     }
 
-    async function onReady () {
+    async function onReady() {
         // this runs when everything is ready
         // aka mapbox loaded and component is mounted
         if (mounted && mapboxLoaded) {
@@ -38,23 +38,57 @@
             map = new mapboxgl.Map({
                 container: 'map',
                 style: 'mapbox://styles/mapbox/light-v10',
-                // why 40.7128 , 74.0060? b/c we will get coordinates later; 
+                // why 0,0 b/c we will get coordinates later; 
                 // so ppl will see a map somewhere even if there are not coordinates available
-                center: [40.7128, -74.0060],
-                zoom: 10
+                center: [0, 0],
+                zoom: 1
             });
+            
+            locate()
         }
     }
 
-    async function locate () {
+    async function locate() {
         location = await hospitalLocator.getLocationFromBrowser();
 
         map.flyTo({
-            center: [location.longitude,location.latitude]
+            center: [location.longitude,location.latitude],
+            zoom: 10
         });
+    }
+
+    async function addHospitalMarkers() {
+        const hospitalList = await hospitalLocator.getHospitalsNearby();
+
+        hospitalList.elements.forEach(item => {
+            
+            let latitude;
+            let longitude;
+
+            if (item.lat) {
+                latitude = item.lat;
+                longitude = item.lon;
+            }
+            else {
+                latitude = item.center.lat;
+                longitude = item.center.lon;
+            }
+
+            addHospitalMarker(map,latitude,longitude);
+        })
+    }
+
+    function addHospitalMarker(map, latitude, longitude) {
+
+        var coordinates = [longitude, latitude];
+
+        var marker = new mapboxgl.Marker()
+            .setLngLat(coordinates)
+            .addTo(map);
     }
 </script>
 
 <div id="map"></div>
 
 <button on:click={locate}>Locate</button>
+<button on:click={addHospitalMarkers}>add markers</button>
