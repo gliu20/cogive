d181447920ba663ef554f25aff9bff39f66a7ee1
George <gliu10000@gmail.com>
2020-05-04 13:54:35 -0400

    refactor map code to hospitalmap

--- src/HospitalLocator.svelte
+++ src/HospitalLocator.svelte
@@ -31,54 +31,6 @@
 
     // TO-DO: FIND LAT AND LONG FROM COUNTRY, STATE, CITY
 
-    // Function to create a map
-    function makeMap(lat, long) {
-        mapboxgl.accessToken = "pk.eyJ1IjoibWVlLWtlbGw0MiIsImEiOiJjazlwdXl1NWIwZWZuM25tZXhkMXYxMjFmIn0.6vChDLXY_PeIkScr6c6otQ"
-
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/light-v10',
-            center: [long, lat],
-            zoom: 10
-        });
-
-        return map;
-    }
-
-    // Function to add a marker at a hospital coordinate
-    function addHospitalMarker(map, lat, long) {
-
-        var coordinates = [lat, long];
-
-        // Create a HTML element for each marker
-        var mark = document.createElement('div');
-        mark.className = 'marker';
-
-        // Add to map
-        new mapboxgl.Marker(mark)
-            .setLngLat(coordinates)
-            .addTo(map)
-
-    }
-
-
-    // There might be a better way of doing this in Svelte but idk
-
-    async function displayHospitals() {
-
-        var hospitalDirectory = await getNearbyHospitals(latitude, longitude);
-        var text = document.getElementById('hospitals');
-        text.innerText = hospitalDirectory;
-
-        const map = await makeMap(latitude, longitude);
-        for (hospital in hospitalDirectory) {
-            addHospitalMarker(map, hospital[0], hospital[1]);
-        }
-
-    }
-
-    displayHospitals();
-    console.log(longitude + " " + latitude);
 
 </script>
 
