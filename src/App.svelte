294c76f16a7818b075402715fcbd95ed81c2c4b3
gliu20 <gliu10000@gmail.com>
2020-05-05 16:20:13 -0400

    add navbar

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,9 @@
 	import HospitalLocator from "./components/HospitalLocator.svelte";
 	import Responsive from "./components/Responsive.svelte";
 	import Footer from "./components/Footer.svelte";
+	import NavBar from "./components/NavBar.svelte";
 </script>
 
+<NavBar></NavBar>
 <HospitalLocator></HospitalLocator>
 <Footer></Footer>
