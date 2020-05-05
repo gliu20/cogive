339e3650669b1269e6520b9fd1c4074d50e365c9
gliu20 <gliu10000@gmail.com>
2020-05-05 15:50:14 -0400

    create navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,9 @@
+<div class="nav">
+    <ul class="navbar">
+        <li class="nav-element"><a href="#" class="nav-link">About</a></li>
+        <li class="nav-element"><a href="#" class="nav-link">Donate PPE</a></li>
+        <li class="nav-element"><a href="#" class="nav-link">Build PPE</a></li>
+        <li class="nav-element"><a href="#" class="nav-link">Fundraise</a></li>
+    </ul>
+    <img src="logo.png" id="navLogo" alt="logo">
+</div>
