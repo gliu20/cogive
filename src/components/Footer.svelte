bbb468a4ea11eaff6cf2f698a0d2f88831a7b959
gliu20 <gliu10000@gmail.com>
2020-05-05 16:02:13 -0400

    move footer styles

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,1 +1,16 @@
-<div class="footer">© CoGive 2020</div>
+<script>
+	import Responsive from "./components/Responsive.svelte";
+</script>
+
+<style>
+/* Footer */
+.footer {
+    width: 100%;
+    text-align: center;
+    padding: 15px;
+}
+</style>
+
+<Responsive color="#fff" background="#333">
+    <footer class="footer">© CoGive 2020</footer>
+</Responsive>
