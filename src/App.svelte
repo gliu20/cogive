cf4db4b8015a7aa367688925e024efaf6a122081
gliu20 <gliu10000@gmail.com>
2020-05-05 16:08:36 -0400

    add footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script>
 	import HospitalLocator from "./components/HospitalLocator.svelte";
+	import Responsive from "./components/Responsive.svelte";
+	import Footer from "./components/Footer.svelte";
 </script>
 
 <HospitalLocator></HospitalLocator>
+<Footer></Footer>
