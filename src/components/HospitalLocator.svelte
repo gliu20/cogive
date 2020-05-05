8777bb12a4b4ef765ddd664182ef67fbb2b1d7b8
gliu20 <gliu10000@gmail.com>
2020-05-05 12:19:47 -0400

    extract out hospital locator view

---
+++ src/components/HospitalLocator.svelte
@@ -0,0 +1,14 @@
+<script>
+    import HospitalMap from "./HospitalMap.svelte";
+</script>
+
+<style>
+    section {
+        display:flex;
+    }
+</style>
+
+<section>
+    <HospitalMap></HospitalMap>
+    <h2>Find a hospital in need nearby.</h2>
+</section>
