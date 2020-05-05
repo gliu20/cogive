824c17f16de9e5675720a06838c726cddaa7224d
gliu20 <gliu10000@gmail.com>
2020-05-05 16:01:57 -0400

    add responsive component

---
+++ src/components/Responsive.svelte
@@ -0,0 +1,19 @@
+<!-- just a container helper -->
+
+<script>
+    export let background = "#ffffff";
+    export let color = "#333333";
+</script>
+
+<style>
+    .container {
+        margin:auto;
+    }
+    @media (min-width: 700px) { .container {width:650px;} }
+    @media (min-width: 1000px) { .container {width:900px;} }
+    @media (min-width: 1200px) { .container {width:1100px;} }
+</style>
+
+<div class="container" style="{`background-color:${background};`} {`color:${color};`}">
+    <slot></slot>
+</div>
