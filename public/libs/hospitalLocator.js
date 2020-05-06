db725d458ae0f9f79c60416f6d8fbe077c89b031
George <gliu10000@gmail.com>
2020-05-05 21:45:00 -0400

    disable  lgeolocation timeout

--- public/libs/hospitalLocator.js
+++ public/libs/hospitalLocator.js
@@ -23,7 +23,7 @@ hospitalLocator._getLocationFromBrowser = function () {
 
     const options = {
         enableHighAccuracy: true,
-        timeout: 5000,
+        //timeout: 5000,//disable bc need wait for user
         maximumAge: 60000 // 60 seconds
     };
 
